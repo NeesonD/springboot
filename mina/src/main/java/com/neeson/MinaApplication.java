fe723df3288edd26eec2ee741753376783a630a4
neeson <d1043358774@gmail.com>
2017-08-19 11:33:44 +0800

    新增mina整合

---
+++ mina/src/main/java/com/neeson/MinaApplication.java
@@ -0,0 +1,12 @@
+package com.neeson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinaApplication.class, args);
+	}
+}
